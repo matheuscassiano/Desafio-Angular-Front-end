00b561924cc3f72327dcb177794d95d568bc732a
Matheus Cassiano <matheuscassiano9@gmail.com>
2022-12-16 01:08:13 +0000

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PlaylistsComponent } from './pages/playlists/playlists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'playlists', component: PlaylistsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
