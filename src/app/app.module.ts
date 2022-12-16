45cf828ff05a08f7388e61599135322514810183
Matheus Cassiano <matheuscassiano9@gmail.com>
2022-12-16 01:08:26 +0000

    chore: add components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './pages/home/home.module';
+
+import { HttpClientModule } from '@angular/common/http';
+import { PlaylistsModule } from './pages/playlists/playlists.module';
+import { ListModule } from './components/list/list.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    HttpClientModule,
+    PlaylistsModule,
+    ListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
